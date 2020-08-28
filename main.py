4ed7c9b1b1a09408910086aba2696a7c27c9a835
JoelJohn08 <jmj6182@psu.edu>
2020-08-28 10:03:40 +0000

    spell

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-celsius = input("Enter temperatue in celsius: ")
+celsius = input("Enter temperature in celsius: ")
 celsius = float(celsius)
 fahrenheit = (celsius*1.8)+32;
 print(f"{celsius}° in Celsius is equivalent to {fahrenheit}° Fahrenheit.")
